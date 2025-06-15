f1391d66b583c83433c1efa40d1181bbfbed9059
Matheuszaogoat <mdasilvadeoliveira035@gmail.com>
2025-06-15 11:54:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("o mior dev de todos os tempos");
+
+let nome = "Matheus";
+const idade = 17;
+let jogadorOnline = true;
+let gostaDeJavaScript = "Concerteza."
+
+console.log("Nome do jogador:", nome);
+console.log("Idade do jogador:", idade);
+console.log("Está online?", jogadorOnline);
+console.log("Gosta de JavaScript?", gostaDeJavaScript);
+
+alert("Olá, " + nome + "! Você tem " + idade + " anos e gosta de JavaScript? " + gostaDeJavaScript);
+
+let numero1 = 10;
+let numero2 = 5;
+let soma = numero1 + numero2;
+let diferença = numero1 - numero2;
+
+console.log("Soma:", soma)
